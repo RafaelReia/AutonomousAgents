355a6c48bd3e7ea4a40a52fdb02d1e134a388268
Yikang Wang <tjbwyk@gmail.com>
2014-11-20 12:45:40 +0100

    Policy Evaluation refactored

--- src/main/PolicyEvaluation.java
+++ src/main/PolicyEvaluation.java
@@ -1,5 +1,8 @@
 package main;
 
+import static main.BasicEnvironment.WORLDSIZE;
+import static agents.Agent.*;
+
 import java.util.ArrayList;
 
 import agents.Predator;
@@ -15,34 +18,96 @@ public class PolicyEvaluation extends BasicEnvironment {
 
 	// Constructor for multiple predators/preys
 	public PolicyEvaluation(ArrayList<Predator> predators_,
-			ArrayList<Prey> preys_) {
-		super(predators_, preys_);
+			Prey prey_) {
+		super(predators_, prey_);
 	}
 
 	public int run() {
-		int time = 0;
-		
-		for(Prey prey:preys)
-			//prey.print();
+		double[][][][] values = evaluatePolicy(prey);
 		
 		for (Predator predator : predators) {
-			//predator.print();
-			//System.out.println();
-			PredatorPE predatorPE = new PredatorPE(predator);
-			double[][][][] values = predatorPE.evaluatePolicy(preys.get(0),this);
-			
-			preys.get(0).print();
+			prey.print();
 			predator.print();
-			System.out.println(", Evaluation = "+values[predator.getX()][predator.getY()][preys.get(0).getX()][preys.get(0).getY()]);
-			//System.out.println("TEMP= "+values[2][3][5][4]);
-			//System.out.println("TEMP= "+values[2][10][10][10]);
-			//System.out.println("TEMP= "+values[10][10][0][0]);
-			//printValues(values);
+			System.out.println(", Evaluation = "+values[predator.getX()][predator.getY()][prey.getX()][prey.getY()]);
 		}
-		
-		return time;
+		return 0;
 	}
 
+
+	public double[][][][] evaluatePolicy(Prey prey) {
+		// Create array which stores values for all fields in the grid
+		// values array is initialized: v=0 for every state
+		double[][][][] values = new double[WORLDSIZE][WORLDSIZE][WORLDSIZE][WORLDSIZE];
+
+		double delta = 100.0;
+		double theta = 1e-6;
+		// Repeat until values have converged
+		int count = 0;
+		while (delta > theta) {
+			delta = 0.0;
+			for (int x = 0; x < WORLDSIZE; x++) {
+				for (int y = 0; y < WORLDSIZE; y++) {
+					for (int px = 0; px < WORLDSIZE; px++) {
+						for (int py = 0; py < WORLDSIZE; py++) {
+							double temp = values[x][y][px][py];
+
+							values[x][y][px][py] = sumActions(values, x, y, px,
+									py, prey);
+							// Updating delta, which is used for the convergence
+							// check
+							delta = Math.max(delta,
+									Math.abs(temp - values[x][y][px][py]));
+							//System.out.println(x + " " + y + " " + px + " " + py + ": " + delta);
+						}
+					}
+				}
+			}
+			count++;
+			System.out.println(delta);
+			
+		}
+		System.out.println("Number of iterations: " + count);
+		return values;
+	}
+
+	private double prob(int a) {
+		return 0.2;
+	}
+
+	/*
+	 * Helper function which loops over actions to and sum the values of all
+	 * possible actions from that state. Output is the sum.
+	 */
+	public double sumActions(double[][][][] values, int posX, int posY,
+			int px, int py, Prey prey) {
+
+		double output = 0.0;
+		int bestDirection = 0;
+		double gamma = 0.8; // TODO
+
+		for (int a = 0; a < DIR_NUM; a++) {
+
+			// Calculate reward of taking action a to go to s'
+			// Calculate value of s'
+			
+			for (int aPrey = 0; aPrey < DIR_NUM; aPrey++) {
+				Predator dummyPredator = new Predator(posX, posY);
+				Prey dummyPrey = new Prey(px, py);
+				
+				double actionReward = getReward(dummyPredator,dummyPrey,a,aPrey);
+
+				dummyPredator.move(a);
+				dummyPrey.move(aPrey);
+				
+				double value = values[dummyPredator.getX()][dummyPredator.getY()][dummyPrey.getX()][dummyPrey.getY()];
+				output += this.prob(a) * prey.prob(getPredators() ,aPrey)*(actionReward + gamma * value);
+			}
+
+		}
+		
+		return output;
+	}
+	
 	private void printValues(double[][][][] values) {
 		for (int x = 0; x < WORLDSIZE; x++) {
 			for (int y = 0; y < WORLDSIZE; y++) {
